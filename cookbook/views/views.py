df44bc518e1870190979c8c3f4dc78f06e536f82
rz65hege <105251103+rz65hege@users.noreply.github.com>
2022-08-17 02:00:11 +0200

    Update views.py

--- cookbook/views/views.py
+++ cookbook/views/views.py
@@ -142,7 +142,8 @@ def get_prediction(pk):
         url = settings.API_URL + 'predict_times/'
         headers = {'Content-Type': 'application/json'}
 
-        payload = { "recipe_text": getattr(recipe[0], "description"), "ingredients": []}
+        #payload = { "recipe_text": getattr(recipe[0], "description"), "ingredients": []}
+        payload = { "recipe_text": "text", "ingredients": []}
         payload["ingredients"].extend(food)
 
         response = requests.post(url, json = payload, headers=headers)
